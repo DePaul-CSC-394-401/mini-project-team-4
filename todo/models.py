8a214ee9065c75604da3662f53fad121d7df541a
Fritz Nastor <98058294+fritznastor@users.noreply.github.com>
2024-09-15 23:44:56 -0500

    Fritz Nastor: completed FR 4,7, and 9

--- todo/models.py
+++ todo/models.py
@@ -3,9 +3,14 @@ from django.utils import timezone
 
 
 class Todo(models.Model):
+
+    PRIORITY_CHOICES = [('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High'),]
+
     title = models.CharField(max_length=100)
     details = models.TextField()
     date = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='Medium')
 
     def __str__(self):
         return self.title
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-
-# import todo form and models
-
 from .forms import TodoForm
 from .models import Todo
 
@@ -10,14 +7,14 @@ from .models import Todo
 
 
 def index(request):
-
     item_list = Todo.objects.order_by("-date")
     if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('todo')
-    form = TodoForm()
+    else:
+        form = TodoForm()
 
     page = {
         "forms": form,
@@ -27,9 +24,38 @@ def index(request):
     return render(request, 'todo/index.html', page)
 
 
+
 ### function to remove item, it receive todo item_id as primary key from url ##
 def remove(request, item_id):
     item = Todo.objects.get(id=item_id)
     item.delete()
     messages.info(request, "item removed !!!")
+    return redirect('todo')
+
+
+def edit(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Item updated successfully!")
+            return redirect('todo')
+    else:
+        form = TodoForm(instance=item)
+
+    page = {
+        "forms": form,
+        "title": "Edit TODO Item",
+        "item_id": item_id,
+    }
+    return render(request, 'todo/edit.html', page)
+
+
+
+
+def mark_complete(request, item_id):
+    item = get_object_or_404(Todo, id=item_id)
+    item.completed = True
+    item.save()
     return redirect('todo')

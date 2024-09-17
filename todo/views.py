e77e0e192fc8b3b0dbb8bddecc2e17a32053c868
Fritz Nastor <98058294+fritznastor@users.noreply.github.com>
2024-09-17 01:59:11 -0500

    sort by highest to lowest priority and nearest to farthest due date

--- todo/views.py
+++ todo/views.py
@@ -5,15 +5,10 @@ from django.contrib import messages
 from django.urls import reverse_lazy
 from .forms import TodoForm
 from django.db.models import Q
-from django.views.generic.edit import FormView
 from .models import Todo
-
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login
-
-from .models import Todo
 
 class CustomLoginView(LoginView):
     template_name = 'todo/login.html'
@@ -35,26 +30,41 @@ class RegisterPage(FormView):
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
     
-    def get(self,*args, **kwargs):
+    def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('todo')
         return super(RegisterPage, self).get(*args, **kwargs)
 
-    
-
 @login_required
 def index(request):
+    # Get the search query and sort option from the request
+    query = request.GET.get('q', None)
+    sort_by = request.GET.get('sort_by', None)
+
     # Filter todos by the logged-in user
-    item_list = Todo.objects.filter(user=request.user).order_by("-date")
+    item_list = Todo.objects.filter(user=request.user)
+
+    if query:
+        # Filter items by title or description (details) if a search query exists
+        item_list = item_list.filter(Q(title__icontains=query) | Q(details__icontains=query))
+
+    # Apply sorting based on the selected option
+    if sort_by == 'priority':
+        # Sort by priority: highest ('High') to lowest ('Low')
+        priority_order = {'High': 1, 'Medium': 2, 'Low': 3}
+        item_list = sorted(item_list, key=lambda x: priority_order[x.priority])
+    elif sort_by == 'date':
+        # Sort by date: nearest to farthest due date
+        item_list = item_list.order_by('date')
+    else:
+        # Default sorting by creation date descending
+        item_list = item_list.order_by('-date')
 
     if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
-            # Don't save directly yet, but create an instance without committing to DB
             todo_item = form.save(commit=False)
-            # Assign the currently logged-in user to the todo item
             todo_item.user = request.user
-            # Now save the todo item to the database
             todo_item.save()
             return redirect('todo')
     else:
@@ -68,8 +78,6 @@ def index(request):
     return render(request, 'todo/index.html', page)
 
 
-
-### function to remove item, it receive todo item_id as primary key from url ##
 @login_required
 def remove(request, item_id):
     item = get_object_or_404(Todo, id=item_id, user=request.user)
@@ -107,33 +115,3 @@ def mark_complete(request, item_id):
     item.completed = True
     item.save()
     return redirect('todo')
-
-
-@login_required
-def index(request):
-    # Get the search query from the request
-    query = request.GET.get('q', None)
-
-    # Filter todos by the logged-in user and search query if present
-    item_list = Todo.objects.filter(user=request.user).order_by("-date")
-
-    if query:
-        # If a search query exists, filter the items by title or description (details)
-        item_list = item_list.filter(Q(title__icontains=query) | Q(details__icontains=query))
-
-    if request.method == "POST":
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            todo_item = form.save(commit=False)
-            todo_item.user = request.user
-            todo_item.save()
-            return redirect('todo')
-    else:
-        form = TodoForm()
-
-    page = {
-        "forms": form,
-        "list": item_list,
-        "title": "TODO LIST",
-    }
-    return render(request, 'todo/index.html', page)
